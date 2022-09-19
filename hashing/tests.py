b5c8782978fc750d5a8fae9a5d4e15329c2408ea
Rafe Stefano <stefanorafe@gmail.com>
2022-09-19 21:15:39 +0200

    finished func tests

--- hashing/tests.py
+++ hashing/tests.py
@@ -3,33 +3,26 @@ from django.test import TestCase
 import time
 
 
-class FunctionalTestCase(TestCase):
-    def setUp(self):
-        self.browser = webdriver.Chrome()
-    def tearDown(self):
-        self.browser.quit()
-
-    def test_homepage(self):
-        self.browser.get('http://127.0.0.1:8000/')
-        time.sleep(1)
-        assert self.browser.page_source.find("Enter hash here")
-
-
-
-
-
-
-
-
-
-
-
-
 class UnitTestCase():
     pass
 
 
 
 
+# class FunctionalTestCase(TestCase):
+#     def setUp(self):
+#         self.browser = webdriver.Chrome()
+#     def tearDown(self):
+#         self.browser.quit()
 
+#     def test_homepage(self):
+#         self.browser.get('http://127.0.0.1:8000/')
+#         time.sleep(1)
+#         assert self.browser.page_source.find("Eter here bnbhgfh")
 
+#     def test_hash_of_hello(self):
+#         self.browser.get('http://127.0.0.1:8000/')
+#         text = self.browser.find_element_by_id('id_text')
+#         text.send_keys('hello')
+#         self.browser.find_element_by_name("submit").click()
+#         self.assertInHTML('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
